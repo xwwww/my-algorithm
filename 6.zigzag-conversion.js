6531f43e1f26a8ef0cd6079a90ac1b64fc24bb7c
xiawei07 <xiawei07@58.com>
2021-05-26 15:48:30 +0800

    Z 字形变换

--- 6.zigzag-conversion.js
+++ 6.zigzag-conversion.js
@@ -11,6 +11,24 @@
  * @return {string}
  */
 var convert = function(s, numRows) {
-
+    const arr = []
+    let flag = -1
+    let i = 0
+    for (let j = 0; j < s.length; j++) {
+        if (!arr[i]) {
+            arr[i] = []
+        }
+        arr[i].push(s.charAt(j))
+        if (i === 0 || i === numRows - 1) {
+            flag *= -1
+        }
+        i += flag
+    }
+    let result = ''
+    for (let k = 0; k < arr.length; k++) {
+        result += arr[k].join('')
+    }
+    return result
 };
 // @lc code=end
+convert('PAYPALISHIRING', 4)
